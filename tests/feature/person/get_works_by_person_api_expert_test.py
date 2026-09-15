5f531863fbd01496ad56bbcb4784055fe6a7c969
Esteban <estebancg12@hotmail.com>
2026-09-15 14:13:33 +0000

    fix: formatting and adding error handling tests

--- tests/feature/person/get_works_by_person_api_expert_test.py
+++ tests/feature/person/get_works_by_person_api_expert_test.py
@@ -4,10 +4,12 @@ from quyca.infrastructure.mongo import database
 
 random_person_id = database["person"].aggregate([{"$sample": {"size": 1}}]).next()["_id"]
 
+
 def test_get_works_csv_by_person(client):
     response = client.get(f"/person/{random_person_id}/research/products?max=10&page=1&sort=citations_desc")
 
     assert response.status_code == 200
+
 
 @patch("quyca.domain.services.news_service.get_news_by_person")
 def test_get_person_research_products_filters_returns_400_on_error(mock_service, client):
